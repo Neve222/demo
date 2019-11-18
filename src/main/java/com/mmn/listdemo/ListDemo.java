82ea02961e59e3ea32e574cbfa1789616146a5d8
mamengni <mamengni@treefinance.com.cn>
2019-11-18 19:32:02 +0800

    List基本方法

--- src/main/java/com/mmn/listdemo/ListDemo.java
+++ src/main/java/com/mmn/listdemo/ListDemo.java
@@ -1,10 +1,9 @@
 package com.mmn.listdemo;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
+ * List的基本方法
  * @author mmn
  * @date 2019-11-17
  */
@@ -16,6 +15,32 @@ public class ListDemo {
     private static ListIterator<String> lit;
 
     public static void basicTest(List<String> a) {
+        a.add("x");
+        a.add(1, "c");
 
+
+        b = a.contains("1");
+        System.out.println("------1-----" + b);
+        s = a.get(1);
+        System.out.println("------2-----" + s);
+        i = a.indexOf("1");
+        System.out.println("------3-----" + i);
+        b = a.isEmpty();
+        System.out.println("------4-----" + b);
+        it = a.iterator();
+        while (it.hasNext()) {
+            System.out.println("-------it-----" + it.next());
+        }
+        lit = a.listIterator();
+
+        System.out.println("------6-----" + lit);
+        lit = a.listIterator(2);
+        System.out.println("------7-----" + lit);
+
+    }
+
+
+    public static void main(String[] args) {
+        basicTest(new ArrayList<String>(Arrays.asList("a", "b")));
     }
 }
